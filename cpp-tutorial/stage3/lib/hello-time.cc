c0167ca3384fd97424a80ae35bb800de0026c174
Sully <ssuleiman955@gmail.com>
2021-10-18 17:50:38 -0500

    introducing some bad code

--- cpp-tutorial/stage3/lib/hello-time.cc
+++ cpp-tutorial/stage3/lib/hello-time.cc
@@ -1,8 +1,13 @@
 #include "lib/hello-time.h"
 #include <ctime>
 #include <iostream>
+#include <string_view>
 
 void print_localtime() {
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
+  
+  std::string s = "Hellooooooooooooooo ";
+  std::string_view sv = s + "World\n";
+  std::cout << sv;
 }
