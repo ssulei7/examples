9928d88c99f9bc34cfc63af7a2ce8d7a4adb41bb
Sully <ssuleiman955@gmail.com>
2021-10-18 18:10:05 -0500

    Update hello-time.cc

--- cpp-tutorial/stage3/lib/hello-time.cc
+++ cpp-tutorial/stage3/lib/hello-time.cc
@@ -1,13 +1,8 @@
 #include "lib/hello-time.h"
 #include <ctime>
 #include <iostream>
-#include <string_view>
 
 void print_localtime() {
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
-  
-  std::string s = "Hellooooooooooooooo ";
-  std::string_view sv = s + "World\n";
-  std::cout << sv;
 }
